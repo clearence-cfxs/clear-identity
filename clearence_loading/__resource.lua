d1ff273225dc34a2811f178cee2231affa2440a4
clearence-cfxs <80207115+clearence-cfxs@users.noreply.github.com>
2021-03-07 17:31:07 +0300

    Add files via upload

---
+++ clearence_loading/__resource.lua
@@ -0,0 +1,17 @@
+files {
+    'index.html',
+    'credit.png',
+    'loadscreen.jpg',
+    'logo.png',
+    'options.png',
+    'overlay-menu.png',
+    'quit.png',
+    'siteweb.png',
+    'start.png',
+    'style.css',
+	'music/Loading.ogg'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
